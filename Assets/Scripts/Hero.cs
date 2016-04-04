47b1bf3601888df758603a809804b38d7441de14
vincent <pk0119@gmail.com>
2016-04-03 18:04:16 -0700

    Initial status

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+
+	float unityx;
+	float unityy;
+	Vector3 heroPos;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+}
